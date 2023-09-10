89a9642a03e64db7fe2b1ed48116a07d9ea4402a
Master Pampa <styven.munhurrun@gmail.com>
2023-09-10 21:10:52 +0200

    responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './header.css'
 function Header () {
     return(
         <header>
-            <i className="fa-solid fa-2xl fa-bars"></i>
+            <i class="fa-solid fa-house fa-2xl"></i>
             <div className='links'>
                 <i className="fa-brands fa-2xl fa-github"></i>
                 <i className="fa-solid fa-2xl fa-envelope"></i>
