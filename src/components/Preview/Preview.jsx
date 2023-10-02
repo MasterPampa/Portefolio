23bab468c8e4abb30c031d7c26f34b1a83740cb7
Master Pampa <styven.munhurrun@gmail.com>
2023-10-02 12:01:16 +0200

    Nouveaux projets, et optimisation du seo

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -3,11 +3,12 @@ import data from '../../data/data.json'
 import React from 'react';
 import { useEffect, useState } from 'react';
 
-import image1 from '../../images/welcome.jpg'
-import image2 from '../../images/Booki.png'
-import image3 from '../../images/Kasa.png'
-import image4 from '../../images/ArgentBank.png'
-import image5 from '../../images/77Events.png'
+import image1 from '../../images/welcome.webp'
+import image2 from '../../images/ArgentBank.webp'
+import image3 from '../../images/Kasa.webp'
+import image4 from '../../images/NinaCarducci.webp'
+import image5 from '../../images/Ohmyfood.webp'
+import image6 from '../../images/Booki.webp'
 
 import redux from '../../images/redux.svg'
 import html from '../../images/html.png'
@@ -15,6 +16,7 @@ import css from '../../images/css.png'
 import js from '../../images/js.png'
 import react from '../../images/react.png'
 import sass from '../../images/sass.png'
+import lighthouse from '../../images/lighthouse.svg'
 
 const images = {
   0: image1,
@@ -22,19 +24,21 @@ const images = {
   2: image3,
   3: image4,
   4: image5,
+  5: image6,
 }
 const figcaption = {
-  1: <div><img src={html}></img><img src={css}></img></div>,
+  1: <div><img src={react}></img><img src={redux}></img><img src={sass}></img></div>,
   2: <div><img src={html}></img><img src={sass}></img><img src={js}></img></div>,
-  3: <div><img src={react}></img><img src={redux}></img><img src={sass}></img></div>,
-  4: <img src={react}></img>
+  3: <div><img src={lighthouse}></img><img src={sass}></img><img src={html}></img></div>,
+  4: <div><img src={html}></img><img src={css}></img></div>,
+  5: <div><img src={html}></img><img src={css}></img></div>,
 }
 const links = {
   0: "",
   1: "https://masterpampa.github.io/Projet3_OpenClassrooms/Booki/",
-  2: "",
-  3: "",
-  4: ""
+  2: "https://masterpampa.github.io/Kasa/",
+  3: "https://masterpampa.github.io/Ninacarducci",
+  4: "https://masterpampa.github.io/Ohmyfood"
 }
 
 function Preview({ slide, isFadingOut }) {
