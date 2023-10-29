5e4cd2b46582aa684df0df3932fabb035a0c2f82
Master Pampa <styven.munhurrun@gmail.com>
2023-10-29 12:32:07 +0100

    fix lien home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header({ changeLanguage, language }) {
 
   return (
     <header>
-      <a href='/'><i className="fa-solid fa-house fa-2xl"></i></a>
+      <a href='https://masterpampa.github.io/Portefolio/'><i className="fa-solid fa-house fa-2xl"></i></a>
       <div className='links'>
         <a href='https://github.com/MasterPampa' target="_blank"><i className="fa-brands fa-2xl fa-github"></i></a>
         <a target="_top" href="mailto:styvenmunhurrun@gmail.com"><i className="fa-solid fa-2xl fa-envelope"></i></a>
